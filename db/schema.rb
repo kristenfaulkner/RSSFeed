b9de1bf12c1dd12347cd67cb4c823fb80cae88fa
App Academy Student <student@appacademy.io>
2014-07-17 01:26:06 -0700

    added userId to Feed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717071447) do
+ActiveRecord::Schema.define(version: 20140717082300) do
 
   create_table "entries", force: true do |t|
     t.string   "guid",         null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140717071447) do
     t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "feeds", ["url"], name: "index_feeds_on_url", unique: true
